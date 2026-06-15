975606073afc1124f4fa342bdfdeba9d5ac89ed9
inunuwann <yukiya.0216.5063@gmail.com>
2026-06-15 14:25:20 +0900

    change passwd

--- media.sh
+++ media.sh
@@ -1,1 +1,7 @@
-curl -vL -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'origurl=http%3a%2f%2fwww%2egoogle%2ecom&username=USERNAME&password=PASSWD&ok=ログイン' https://metro-cit.ac.jp:9998/forms/user_login
+curl -vL -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "origurl=http%3a%2f%2fwww%2egoogle%2ecom" \
+  -d "username=m22067" \
+  -d "password=2005956699Masato" \
+  --data-urlencode "ok=ログイン" \
+  https://metro-cit.ac.jp:9998/forms/user_login
